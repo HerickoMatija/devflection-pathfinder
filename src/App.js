18da9ed5b4e8024f617101db9ba978d838c02620
Matija <matija.hericko@gmail.com>
2020-07-31 20:15:23 +0200

    Removed extra div from App

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import Pathfinder from "./Pathfinder/PathFinder";
 
 function App() {
   return (
-    <div className="App">
-      <Pathfinder
-        rows={25}
-        cols={50}
-        startRow={10}
-        startColumn={15}
-        finishRow={10}
-        finishColumn={35}
-      ></Pathfinder>
-    </div>
+    <Pathfinder
+      rows={25}
+      cols={50}
+      startRow={10}
+      startColumn={15}
+      finishRow={10}
+      finishColumn={35}
+    ></Pathfinder>
   );
 }
 
