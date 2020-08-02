356b83a3af0704337f3e3ef27a98fd4021e8a533
Matija <matija.hericko@gmail.com>
2020-08-02 17:45:38 +0200

    Updates

---
+++ precache-manifest.c64f4d126adaa6922c9f8a2e04870e8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f528a0a6df790e81e291c1d9e5393b29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "566eb91bb0204d404747",
+    "url": "/static/css/main.35435dc8.chunk.css"
+  },
+  {
+    "revision": "84af5791a2ec68a3a4a4",
+    "url": "/static/js/2.4feba17e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4feba17e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "566eb91bb0204d404747",
+    "url": "/static/js/main.767bc26a.chunk.js"
+  },
+  {
+    "revision": "85a12c31901e373959c2",
+    "url": "/static/js/runtime-main.a544320e.js"
+  },
+  {
+    "revision": "3e5c6643496b9b8f3aeca3a678f088d4",
+    "url": "/static/media/finish.3e5c6643.svg"
+  },
+  {
+    "revision": "c47f04be709071e2ccba09738315f403",
+    "url": "/static/media/start.c47f04be.svg"
+  }
+]);
